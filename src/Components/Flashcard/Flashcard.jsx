bc911219b62c2ea61a46ed7a024677395962c01a
Shanti Rai <shantirai@MacBookAir.lan>
2025-01-23 20:16:05 +0000

    Used prev and managed to get an object that toggles back and forth from Q and A. it also renders correctly atm.

--- src/Components/Flashcard/Flashcard.jsx
+++ src/Components/Flashcard/Flashcard.jsx
@@ -1,10 +1,9 @@
 
-export default function Flashcard({value}) {
+export default function Flashcard({ value, handleClick }) {
 
     return (
-        <section style={{ border: "2px solid black", padding: "10px", margin: "10px" }} >
+        <section onClick ={handleClick} style ={{ border: "2px solid black", padding: "10px", margin: "10px" }} >
             {value}
     </section>
     )
-    
 }
--- src/Components/FlashcardsContainer/FlashcardsContainer.jsx
+++ src/Components/FlashcardsContainer/FlashcardsContainer.jsx
@@ -2,32 +2,48 @@ import Flashcard from "../Flashcard/Flashcard"
 import { useState } from "react";
 
 
+// Problem: if i hard code the question and answer, it doesnt make sense. 
+// I want to make this take a question and answer non-hard-coded values
+// try an array of obj as you cna have key value pairs with that- makes sense
+
+const questionsAndAnswers = [
+    { 
+    question: 'Why do you capitalise the function names in React?', 
+    answer: "In React, function names are capitalized because React uses the capitalization to distinguish between regular HTML elements (e.g., <div>, <p>) and React components. React components must start with a capital letter so that JSX can correctly interpret them as user-defined components, rather than native HTML tags." },
+    
+]
+// array so need to use INDEX
+// Obj inside, so need to use DOT/Square notation
+console.log("test: ", questionsAndAnswers[0].question)
+
+
 
 export default function FlashcardsContainer() {
 
-// const [cardFace, setCardFace]= useState({text.prop.question})
+    const [ qOrA, setQorA ]= useState(questionsAndAnswers[0].question)
 
-// function handleClick(){
-// setCardFace ({text.prop.answer})
-// }
+    // when clicked it should change to the next state
+    function handleClick() {
+        // Toggle state between 'question' and 'answer'
+        // callback function that 
+        setQorA((prev) => (
+            prev === questionsAndAnswers[0].question ? questionsAndAnswers[0].answer : 
+            questionsAndAnswers[0].question
+            ));
+    }
 
     return (
-        < div className="FlashcardsContainer" style={{ border: "2px solid black", padding: "10px", margin: "10px" }}>
+        < div className ="FlashcardsContainer" style ={{ border: "2px solid black", padding: "10px", margin: "10px" }}>
             <div>
-                <Flashcard value = "Q1"/>
-                <Flashcard value = "Q2"/>
-                <Flashcard value = "Q3"/>
+                <Flashcard value={qOrA} handleClick={handleClick}/>
+                <Flashcard value={qOrA} handleClick={handleClick}/>
+                <Flashcard value={qOrA} handleClick={handleClick}/>
             </div>
             <div>
-                <Flashcard value = "Q4"/>
-                <Flashcard value = "Q5"/>
-                <Flashcard value = "Q6"/>
+                <Flashcard value={qOrA} handleClick={handleClick}/>
+                <Flashcard value={qOrA} handleClick={handleClick}/>
+                <Flashcard value={qOrA} handleClick={handleClick}/>
             </div>
         </ div >
     )
 }
-
-
-
-
-// section className="FlashcardsContainer" style={{ border: "2px solid black", padding: "10px", margin: "10px" }} 
